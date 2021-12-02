e6972676f05ba0db7b9e87d58c0874655b4730cb
Nícolas Costa <ncolascostaa@gmail.com>
2021-12-01 22:15:29 -0300

    configuracao da lambda

--- optimize.js
+++ optimize.js
@@ -9,31 +9,31 @@ const S3 = new AWS.S3();
 module.exports.handle = async ({ Records: records }, context) => {
   try {
     await Promise.all(
-      records.map(async record => {
+      records.map(async (record) => {
         const { key } = record.s3.object;
 
         const image = await S3.getObject({
           Bucket: process.env.bucket,
-          Key: key
+          Key: key,
         }).promise();
 
         const optimized = await sharp(image.Body)
-          .resize(1280, 720, { fit: "inside", withoutEnlargement: true })
-          .toFormat("jpeg", { progressive: true, quality: 50 })
+          .resize(400, 400, { fit: "inside", withoutEnlargement: true })
+          .toFormat("jpeg", { progressive: true, quality: 100 })
           .toBuffer();
 
         await S3.putObject({
           Body: optimized,
           Bucket: process.env.bucket,
           ContentType: "image/jpeg",
-          Key: `compressed/${basename(key, extname(key))}.jpg`
+          Key: `compressed/${basename(key, extname(key))}.jpg`,
         }).promise();
       })
     );
 
     return {
       statusCode: 301,
-      body: { ok: true }
+      body: { ok: true },
     };
   } catch (err) {
     return err;
